e90d5d69e1379c8431e3a6b04d27fc4ccbf7ff61
Thomaskutty <tkurian2@icloud.com>
2023-05-09 23:41:11 +0530

    basic calculator logic done

--- src/main/java/Operation.java
+++ src/main/java/Operation.java
@@ -10,13 +10,36 @@ public class Operation extends HttpServlet{
 	public void service(HttpServletRequest req, HttpServletResponse res ) throws IOException {
 		int n1 =Integer.parseInt( req.getParameter("n1"));
 		int n2 =Integer.parseInt(req.getParameter("n2"));
-		//String op = req.getParameter("operator");
+		char op = req.getParameter("operator").charAt(0);
 		
-		int sum = n1+n2;
 		
-//		PrintWriter p = res.getWriter();
-//		 p.println("result is " + sum);
-		 System.out.print(sum);
+		
+		
+		
+		
+		PrintWriter p = res.getWriter();
+		/* p.println(op); */
+		if (op=='+') {
+			int sum = n1+n2;
+			p.println("Sum is " + sum);
+		}
+		else if (op=='*') {
+			int product = n1*n2;
+			p.println("Product is " + product);
+		}
+		else if (op=='-') {
+			int diff=n1-n2;
+			p.println("Difference is " + diff);
+		}
+		else if (op=='/') {
+			float quo=n1/n2;
+			p.println("Quotient is " + quo);
+		}
+		else {
+			p.print("No result");
+		}
+		 
+//		 System.out.print(sum);
 				
 	}
 }
